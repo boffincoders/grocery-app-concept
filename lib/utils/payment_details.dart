8fde2e4b12bb7c964eaed2fccd63f33782f5d987
Shri Krishna <manojsethi@live.in>
2021-08-23 17:17:22 +0530

    Bug fix

--- lib/utils/payment_details.dart
+++ lib/utils/payment_details.dart
@@ -1,6 +1,6 @@
 class PaymentGatewayDetails {
   static String stripePublishableKey =
-      "pk_test_51JQ80TSHXCT2tdoIrc3zzBFFkorlBwAjPtxCk0sUWzb9rBwYz6mzziCQEBAvmdIM2ZTcflpjKCfFDrpyhl4sU5Fj000sOJUoOl";
+      "pk_test_51JQ80TSHXCT2tdoIrc3zz******************************";
   static String stripeSecretKey =
-      "sk_test_51JQ80TSHXCT2tdoIsKHlgRozt4lku62YNv3c9ufus8xiNDSccIK0A2i0JDuLA8VJzHxWaRRl1JM3rW4kGH9StLby00qHE9IJ4g";
+      "sk_test_51JQ80TSHXCT2tdoIsKHlgRozt4l******************************";
 }
